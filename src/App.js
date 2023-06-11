967965e6fd8173c60051fd1a6492acb443a817c4
McM <134838581+MortimerMcMorrister@users.noreply.github.com>
2023-06-11 08:01:26 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function Board() {
       </div>
       <div className="board-row">
         <button className="square">4</button>
-        <button className="square">Z</button>
+        <button className="square">ü</button>
         <button className="square">6</button>
       </div>
       <div className="board-row">
